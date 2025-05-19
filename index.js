d261dd50cbe551691d47632a7179213bb05d9d27
Anil <anilkumarkondeboina@gmail.com>
2025-05-19 16:09:26 +0530

    intial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(3000, (req, res) => {
+  console.log("running at 3000");
+});
